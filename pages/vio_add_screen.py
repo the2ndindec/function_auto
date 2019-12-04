417e053ff6648eae7ac1f465811a42cae2d11a8c
lijinglin <the2ndindec@hotmail.com>
2019-12-04 14:16:55 +0800

    增加获取三违详情的方法

--- pages/vio_add_screen.py
+++ pages/vio_add_screen.py
@@ -10,6 +10,9 @@ import allure
 
 from common import read_config
 from pages.base_page import BasePage
+from common.mysql_operation import ConnMysql
+
+conn = ConnMysql()
 
 
 class VioAddScreen(BasePage):
@@ -31,8 +34,13 @@ class VioAddScreen(BasePage):
     area_submit_btn = readeConfigObj.get_config('vioAdd', 'area_submit_btn')
     pop_search = readeConfigObj.get_config('vioAdd', 'pop_search')
     vio_submit_btn = readeConfigObj.get_config('vioAdd', 'vio_submit_btn')
+    vio_date_item = readeConfigObj.get_config('vioAdd', 'vio_date_item')
+    vio_address_item = readeConfigObj.get_config('vioAdd', 'vio_address_item')
+    vio_unit_item = readeConfigObj.get_config('vioAdd', 'vio_unit_item')
+    vio_desc_item = readeConfigObj.get_config('vioAdd', 'vio_desc_item')
+    uploaded_tab = readeConfigObj.get_config('vioAdd', 'uploaded_tab')  # 已上报标签
 
-    CONTROL_LOC = "class>=android.widget.LinearLayout"
+    CONTROL_LOC = "class>=android.widget.LinearLayout"  # 弹窗
 
     def choose_vio_date(self, vio_date_str):
         """选择违章时间"""
@@ -109,3 +117,46 @@ class VioAddScreen(BasePage):
     def submit_vio(self):
         with allure.step('提交三违数据'):
             self.driver.click_element(self.vio_submit_btn)
+
+    def collect_detail_of_vio(self):
+        """获取app中三违详情内容"""
+        detail_dic = {}  # 用于存放详情内容
+        _xpath_loc = "//*[@class = 'android.widget.ScrollView']/android.widget.LinearLayout/android.widget.LinearLayout"  # key-value loc
+        _key_loc = "//android.widget.TextView[@index='0']"  # 属性字段loc
+        _value_loc = "//android.widget.TextView[@index='1']"  # 属性值loc
+        _elements = self.driver.get_elements('xpath', _xpath_loc)  # 查找三违详情相关的key和value
+        for _element in _elements:
+            _key_content = _element.find_element_by_xpath(_key_loc).text[:-1]
+            _value_content = _element.find_element_by_xpath(_value_loc).text
+            detail_dic[_key_content] = _value_content
+        return detail_dic
+
+    def collect_detail_of_vio_from_db(self, vio_date, vio_address, vio_unit, vio_desc):
+        """获取数据库中三违详情内容"""
+        details = {}  # 用于存放风险详情内容
+        tmp_detail_list = []  # 存放临时变量
+        tmp_key_list = ('违章时间', '违章地点', '违章人员', '违章定性', '制止人', '违章单位', '违章分类', '三违级别', '查处单位', '三违事实描述', '备注')
+
+        from data import sql_constants
+        tmp_detail = conn.get_infos(sql_constants.detail_of_vio(vio_date, vio_address, vio_unit, vio_desc))[0]  # 通过查询得到详情内容
+        for _index in tmp_detail:
+            tmp_detail_list.append(_index)
+
+        tmp_detail_list[0] = tmp_detail_list[0][:10]  # 处理辨识时间, 截取年月日
+
+        for _index in tmp_key_list:  # 将数据保存到details中
+            details[_index] = tmp_detail_list[tmp_key_list.index(_index)]
+        return details
+
+    def choose_and_click_vio(self, vio_date, vio_address, vio_unit, vio_desc):
+        """点击指定的三违数据"""
+        _vio_xpath_loc = "//*[@resource-id='com.universal:id/recyclerView']/android.widget.RelativeLayout"
+        _vio_elements = self.driver.get_elements('xpath', _vio_xpath_loc)  # 查询列表中的三违
+        for _element in _vio_elements:
+            _date_text = ''.join(_element.find_element_by_id(self.vio_date_item).text.split())[5:]  # 违章时间
+            _address_text = ''.join(_element.find_element_by_id(self.vio_address_item).text.split())[5:]  # 违章地点
+            _unit_text = ''.join(_element.find_element_by_id(self.vio_unit_item).text.split())[5:]  # 违章单位
+            _desc_text = _element.find_element_by_id(self.vio_desc_item).text    # 三违内容描述
+            if _date_text == vio_date and _address_text == vio_address and _unit_text == vio_unit and _desc_text == vio_desc:
+                _element.click()
+                break
